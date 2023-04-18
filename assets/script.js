f8043d189f105f9a932a829c6ce7ba1a85a58c46
Nneif <nneifemakpunonu@gmail.com>
2023-04-18 18:02:56 -0400

    added code that will color code if the time is current, past, or in the future

--- assets/script.js
+++ assets/script.js
@@ -16,26 +16,42 @@ $(function () {
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
-    saveButtonEl.on("click", function(){
+    saveButtonEl.on("click", function(event){
+      event.preventDefault();
       var text = $(this).siblings(".description").val();
-      // console.log(text);
-      var currentHour = $(this).parent().attr("id");
-      // console.log(currentHour);
-      localStorage.setItem(currentHour, text);
-      console.log(localStorage.getItem(currentHour, text));
+      var scheduledHour = $(this).parent().attr("id");
+
+      localStorage.setItem(scheduledHour, text);
     })
 
-    //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
     //
+    var currentHour = dayjs().format("HH");
+    var hours = [09, 10, 11, 12, 13, 14, 15, 16, 17];
+    $(hours).each(function(i, hour){
+     if(hour < currentHour){
+      console.log("show me")
+      $("#" + hour).addClass("past")
+
+     }
+     else if(hour == currentHour){
+      console.log("show me too")
+      $("#" + hour).addClass("present")
+
+     }
+     else if(hour > currentHour){
+      console.log("show me as well")
+      $("#" + hour).addClass("future")
+     }
+    })
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-    $(currentDateEl).text(formatDate.format("dddd, MMMM DD"));
-  });
+    $(currentDateEl).text(formatDate.format("dddd, MMMM DD hh:mm A"));
+  }); 
